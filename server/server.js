b872bc7533106a54ff17f6d9f163590d8360c9b8
Ramanangee <ramananmaya2000@gmail.com>
2019-08-28 18:11:32 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
 console.log('New user connected');
 
-socket.emit('newMessage', {
-    from: 'Ramanangee',
-    text: 'See you then',
-    createdAt: 123123 
-});
+
 
 socket.on('createMessage', (Message)=>{
     console.log('createMessage', Message);
+    io.emit('newMessage',{
+        from: Message.from,
+        text: Message.text,
+        createdAt: new Date().getTime()
+    });
 });
 
 socket.on('disconnect', ()=>{
